c8f2c420d5ab332ec875b658fa2bd38db134ec38
Marco <marco@marcosmeulders.nl>
2019-11-18 12:24:11 +0100

    prettier formatting

--- src/components/ColumnSelector.js
+++ src/components/ColumnSelector.js
@@ -1,70 +1,91 @@
-import React from 'react'
+import React from "react";
 
 const ColumnSelector = ({ columnDefinition, setColumnDefinition }) => {
+  const adaptShowField = (headerToFind, value, colDef) =>
+    colDef.reduce((acc, field) => {
+      if (field.hasOwnProperty("columns")) {
+        adaptShowField(headerToFind, value, field.columns);
+      }
+      if (field.hasOwnProperty("Header") && field.Header === headerToFind) {
+        field.show = value;
+      }
+      return acc.concat(field);
+    }, []);
 
-    const adaptShowField = (headerToFind, value, colDef) => colDef.reduce((acc, field) => {
-        if (field.hasOwnProperty('columns')) {
-            adaptShowField(headerToFind, value, field.columns)
-        }
-        if (field.hasOwnProperty('Header') && field.Header === headerToFind) {
-            field.show = value
-        }
-        return acc.concat(field)
-    }, [])
+  const adaptOrder = (headerToFind, value, colDef) =>
+    colDef.reduce((acc, field) => {
+      if (field.hasOwnProperty("columns")) {
+        adaptOrder(headerToFind, value, field.columns);
+        field.columns.sort((a, b) => (a.order > b.order ? 1 : -1));
+      }
+      if (
+        field.hasOwnProperty("Header") &&
+        field.hasOwnProperty("order") &&
+        field.Header === headerToFind
+      ) {
+        field.order = value;
+      }
+      return acc.concat(field);
+    }, []);
 
-    const adaptOrder = (headerToFind, value, colDef) => colDef.reduce((acc, field) => {
-        if (field.hasOwnProperty('columns')) {
-            adaptOrder(headerToFind, value, field.columns)
-            field.columns.sort((a, b) => (a.order > b.order) ? 1 : -1)
-        }
-        if (field.hasOwnProperty('Header') && field.hasOwnProperty('order') && field.Header === headerToFind) {
-            field.order = value
-        }
-        return acc.concat(field)
-    }, [])
+  const handleShowChange = (Header, event) =>
+    setColumnDefinition(
+      adaptShowField(Header, event.target.checked, columnDefinition)
+    );
 
-    const handleShowChange = (Header, event) => 
-        setColumnDefinition(adaptShowField(Header, event.target.checked, columnDefinition))
+  const handleOrderChange = (Header, event) =>
+    setColumnDefinition(
+      adaptOrder(Header, event.target.value, columnDefinition)
+    );
 
-    const handleOrderChange = (Header, event) => 
-        setColumnDefinition(adaptOrder(Header, event.target.value, columnDefinition))    
-
-    const makeField = field => {
-        return (
-            <tr key={field.Header}>
-                <td>{field.Header}</td>
-                <td><input
-                    name="show"
-                    type="checkbox"
-                    checked={field.show}
-                    onChange={e => handleShowChange(field.Header, e)} /></td>
-                {field.order ?
-                    <td>
-                        <input
-                            name="show"
-                            type="number"
-                            value={field.order}
-                            onChange={e => handleOrderChange(field.Header, e)} />
-                    </td> :
-                    <td></td>}
-            </tr>
-        )
-    }
-
-    const makeColumn = col => {
-        return col.hasOwnProperty('columns') ? (
-            <>
-                {makeField(col)}
-                {col.columns.map(c => makeColumn(c))}
-            </>
-        )
-            : makeField(col)
-    }
-
+  const makeField = field => {
     return (
-        <table><tbody>{columnDefinition.filter(field => field.hasOwnProperty('Header')).map(col => makeColumn(col))}</tbody></table>
-    )
+      <tr key={field.Header}>
+        <td>{field.Header}</td>
+        <td>
+          <input
+            name="show"
+            type="checkbox"
+            checked={field.show}
+            onChange={e => handleShowChange(field.Header, e)}
+          />
+        </td>
+        {field.order ? (
+          <td>
+            <input
+              name="show"
+              type="number"
+              value={field.order}
+              onChange={e => handleOrderChange(field.Header, e)}
+            />
+          </td>
+        ) : (
+          <td></td>
+        )}
+      </tr>
+    );
+  };
 
-}
+  const makeColumn = col => {
+    return col.hasOwnProperty("columns") ? (
+      <>
+        {makeField(col)}
+        {col.columns.map(c => makeColumn(c))}
+      </>
+    ) : (
+      makeField(col)
+    );
+  };
 
-export default ColumnSelector
+  return (
+    <table>
+      <tbody>
+        {columnDefinition
+          .filter(field => field.hasOwnProperty("Header"))
+          .map(col => makeColumn(col))}
+      </tbody>
+    </table>
+  );
+};
+
+export default ColumnSelector;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,114 +1,134 @@
-import React from 'react'
-import { useColumnOrder, useTable, useFilters, useSortBy, usePagination, useBlockLayout, useResizeColumns } from 'react-table'
-import DefaultColumnFilter from './DefaultColumnFilter'
-import { fuzzyTextFilterFn } from '../utils/filterUtils'
-import Pagination from './Pagination'
+import React from "react";
+import {
+  useColumnOrder,
+  useTable,
+  useFilters,
+  useSortBy,
+  usePagination,
+  useBlockLayout,
+  useResizeColumns
+} from "react-table";
+import DefaultColumnFilter from "./DefaultColumnFilter";
+import { fuzzyTextFilterFn } from "../utils/filterUtils";
+import Pagination from "./Pagination";
 
 const Table = ({ columns, data }) => {
-    const filterTypes = React.useMemo(
-        () => ({
-            fuzzyText: fuzzyTextFilterFn,
-            text: (rows, id, filterValue) => {
-                return rows.filter(row => {
-                    const rowValue = row.values[id]
-                    return rowValue !== undefined
-                        ? String(rowValue)
-                            .toLowerCase()
-                            .startsWith(String(filterValue).toLowerCase())
-                        : true
-                })
-            },
-        }),
-        []
-    )
+  const filterTypes = React.useMemo(
+    () => ({
+      fuzzyText: fuzzyTextFilterFn,
+      text: (rows, id, filterValue) => {
+        return rows.filter(row => {
+          const rowValue = row.values[id];
+          return rowValue !== undefined
+            ? String(rowValue)
+                .toLowerCase()
+                .startsWith(String(filterValue).toLowerCase())
+            : true;
+        });
+      }
+    }),
+    []
+  );
 
-    const defaultColumn = React.useMemo(
-        () => ({
-            Filter: DefaultColumnFilter,
-            minWidth: 20,
-            width: 250,
-            maxWidth: 500,
-        }),
-        []
-    )
+  const defaultColumn = React.useMemo(
+    () => ({
+      Filter: DefaultColumnFilter,
+      minWidth: 20,
+      width: 250,
+      maxWidth: 500
+    }),
+    []
+  );
 
-    const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        page,
-        canPreviousPage,
-        canNextPage,
-        pageOptions,
-        pageCount,
-        gotoPage,
-        nextPage,
-        previousPage,
-        setPageSize,
-        prepareRow,
-        state: { pageIndex, pageSize },
-    } = useTable(
-        {
-            columns,
-            data,
-            defaultColumn, // Be sure to pass the defaultColumn option
-            filterTypes,
-            initialState: { pageIndex: 0 }
-        },
-        useFilters,
-        useSortBy,
-        usePagination,
-        useBlockLayout,
-        useResizeColumns
-    )
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    prepareRow,
+    state: { pageIndex, pageSize }
+  } = useTable(
+    {
+      columns,
+      data,
+      defaultColumn, // Be sure to pass the defaultColumn option
+      filterTypes,
+      initialState: { pageIndex: 0 }
+    },
+    useFilters,
+    useSortBy,
+    usePagination,
+    useBlockLayout,
+    useResizeColumns
+  );
 
-    return (
-        <>
-            <table {...getTableProps()}>
-                <thead>
-                    {headerGroups.map(headerGroup => (
-                        <tr {...headerGroup.getHeaderGroupProps()}>
-                            {headerGroup.headers.map(column => (
-                                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                                    {column.render('Header')}
-                                    {/* Render the columns filter UI */}
-                                    <div
-                                        {...column.getResizerProps()} className={`resizer ${column.isResizing ? 'isResizing' : ''}`}
-                                    />
-                                    <div>{column.canFilter ? column.render('Filter') : null}</div>
-                                    <span>
-                                        {column.isSorted
-                                            ? column.isSortedDesc
-                                                ? ' 🔽'
-                                                : ' 🔼'
-                                            : ' '}
-                                    </span>
-                                </th>
-                            ))}
-                        </tr>
-                    ))}
-                </thead>
-                <tbody {...getTableBodyProps()}>
-                    {page.map(
-                        (row, i) => {
-                            prepareRow(row);
-                            return (
-                                <tr {...row.getRowProps()}>
-                                    {row.cells.map(cell => {
-                                        return (
-                                            <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                                        )
-                                    })}
-                                </tr>
-                            )
-                        }
-                    )}
-                </tbody>
-            </table>
-            <br />
-            <Pagination previousPage={previousPage} nextPage={nextPage} pageCount={pageCount} canNextPage={canNextPage} pageIndex={pageIndex} pageOptions={pageOptions} gotoPage={gotoPage} pageSize={pageSize} setPageSize={setPageSize} canPreviousPage={canPreviousPage} />
-        </>
-    )
-}
+  return (
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render("Header")}
+                  {/* Render the columns filter UI */}
+                  <div
+                    {...column.getResizerProps()}
+                    className={`resizer ${
+                      column.isResizing ? "isResizing" : ""
+                    }`}
+                  />
+                  <div>{column.canFilter ? column.render("Filter") : null}</div>
+                  <span>
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? " 🔽"
+                        : " 🔼"
+                      : " "}
+                  </span>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <br />
+      <Pagination
+        previousPage={previousPage}
+        nextPage={nextPage}
+        pageCount={pageCount}
+        canNextPage={canNextPage}
+        pageIndex={pageIndex}
+        pageOptions={pageOptions}
+        gotoPage={gotoPage}
+        pageSize={pageSize}
+        setPageSize={setPageSize}
+        canPreviousPage={canPreviousPage}
+      />
+    </>
+  );
+};
 
-export default Table
+export default Table;
